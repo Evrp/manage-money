9e364f9cb08d158d58639028d7073caac0e298fb
peerapatmhtech <peerapat.mhtech@gmai.com>
2026-04-14 01:42:21 +0700

    refactor: remove compiled JavaScript files and source maps from shared package and update path alias

--- packages/shared/src/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './types/user.interface';
-export * from './types/category.interface';
-export * from './types/transaction.interface';
-export * from './types/budget.interface';
-export * from './types/slip-upload.interface';
--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,5 +1,5 @@
 export * from './types/user.interface';
-export * from './types/category.interface';
+export { CategoryType, type ICategory } from './types/category.interface';
 export * from './types/transaction.interface';
 export * from './types/budget.interface';
 export * from './types/slip-upload.interface';
