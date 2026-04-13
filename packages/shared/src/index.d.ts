0192846944e18720d58a376a1cbe9e53058e60a2
peerapatmhtech <peerapat.mhtech@gmai.com>
2026-04-14 01:58:07 +0700

    refactor: point shared package alias to index.ts and remove redundant compiled index files

--- packages/shared/src/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { Theme, type IUser } from './types/user.interface';
-export { CategoryType, type ICategory } from './types/category.interface';
-export { TransactionType, type ITransaction, type ISlipData } from './types/transaction.interface';
-export { type IBudget } from './types/budget.interface';
-export { SlipUploadStatus, type ISlipUpload } from './types/slip-upload.interface';
